28b6764972be8101b4ef41d8acb0f6ab1adf2ef1
David Essien <davidessienshare@gmail.com>
2017-11-21 15:58:32 +0100

    Create file that contains OOP implementation

---
+++ index.js
@@ -0,0 +1,55 @@
+const PI = 3.142
+class Shape{
+    constructor(name, color, breadth, length){
+        this.color = color;
+        this.breadth = breadth;
+        this.length = length;
+        this.name = name;
+    }
+
+    introduceSelf(){
+        return 'I am a ${this.name} and I have a height of ${this.length}cm and a width of {this.length} and my color is {this.color}.';
+    }
+
+    calculatePerimeter(){
+        return "I don't have a perimeter yet";
+    }
+
+    calculateArea(){
+        return "I don't have an area yet"
+    }
+}
+
+class  Triangle extends Shape{
+    constructor(name, color, breadth, length, hypothenus){
+        super(name, color, breadth, length);
+        this.hypothenus = hypothenus;
+    }
+
+    calculatePerimeter(){
+        return this.length+this.breadth+this.hypothenus;
+    }
+
+    calculateArea(){
+        return 0.5*this.breadth*this.length;
+    }
+}
+
+class Circle extends Shape{
+    constructor(name, color, breadth, length){
+        super(name, color, breadth, length);
+    }   
+
+    calculatePerimeter(){
+        return 2*PI*this.calculateRadius();
+    }
+
+    calculateArea(){
+        return PI*this.calculateRadius()**2;
+    }
+
+    calculateRadius(){
+        return breadth/2;
+    }
+}
+
