156ab3ab09e79b1288e8816bb54a530c395184cc
David Essien <davidessienshare@gmail.com>
2017-11-21 16:49:05 +0100

    Fix method implentation and add comments

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
+//initialize constant to hold the value of PI
 const PI = 3.142
 class Shape{
+    //constructor for shapes class, takes in the name,color, breadth and length of the shape as parameters
     constructor(name, color, breadth, length){
-        this.color = color;
-        this.breadth = breadth;
-        this.length = length;
-        this.name = name;
+        this.color = color;//color of shape
+        this.breadth = breadth;//breadth/diameter of shape
+        this.length = length;//height/length of shape
+        this.name = name;//type of shape
     }
 
-    introduceSelf(){
+    //describe method where the shape talks about itself
+    describeSelf(){
         return `I am a ${this.name} and I have a height of ${this.length}cm and a width of ${this.breadth}cm and my color is ${this.color}.`;
     }
 
@@ -24,6 +27,10 @@ class  Triangle extends Shape{
     constructor(name, color, breadth, length, hypothenus){
         super(name, color, breadth, length);
         this.hypothenus = hypothenus;
+    }
+    
+    introduceSelf(){
+        return `I am a ${this.name} and I have a height of ${this.length}cm, a width of ${this.breadth}cm, a hypothenus of ${this.hypothenus} and my color is ${this.color}.`;
     }
 
     calculatePerimeter(){
@@ -52,5 +59,9 @@ class Circle extends Shape{
 
     calculateRadius(){
         return this.breadth/2;
+    }
+
+    introduceSelf(){
+        return `I am a ${this.name} and I have a height of ${this.length}cm, a width of ${this.breadth}cm, a radius of ${this.calculateRadius()} and my color is ${this.color}.`;
     }
 }
